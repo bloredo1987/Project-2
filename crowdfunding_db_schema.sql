009a415da9d4114d06e22bedbd50ebc6d988310f
bloredo1987 <131644613+bloredo1987@users.noreply.github.com>
2023-07-31 18:30:11 -0500

    Completed SQL Schema and Analysis

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,72 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/PAcChc
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Drop tables if they exist
+DROP TABLE IF EXISTS "Subcategory";
+DROP TABLE IF EXISTS "Category";
+DROP TABLE IF EXISTS "Contacts";
+DROP TABLE IF EXISTS "Campaign";
+
+-- Create Subcategory table
+
+CREATE TABLE "Subcategory" (
+    "subcategory_id" text   NOT NULL,
+    "subcategory_name" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_Subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+CREATE TABLE "Category" (
+    "category_id" text   NOT NULL,
+    "category_name" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_Category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "Campaign" (
+    "cf_id" INT   NOT NULL,
+    "contact_id" INT   NOT NULL,
+    "company_name" VARCHAR(255)   NOT NULL,
+    "description" VARCHAR(255)   NOT NULL,
+    "goal" NUMERIC   NOT NULL,
+    "pledged" NUMERIC   NOT NULL,
+    "outcome" VARCHAR(255)   NOT NULL,
+    "backers_count" INT   NOT NULL,
+    "country" VARCHAR(5)   NOT NULL,
+    "currency" VARCHAR(5)   NOT NULL,
+    "launched_date" DATE   NOT NULL,
+    "end_date" DATE   NOT NULL,
+    "category_id" text   NOT NULL,
+    "subcategory_id" text   NOT NULL,
+    CONSTRAINT "pk_Campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+CREATE TABLE "Contacts" (
+    "contact_id" INT   NOT NULL,
+    "first_name" VARCHAR(255)   NOT NULL,
+    "last_name" VARCHAR(255)   NOT NULL,
+    "email" text   NOT NULL,
+    CONSTRAINT "pk_Contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "Contacts" ("contact_id");
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "Category" ("category_id");
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "Subcategory" ("subcategory_id");
+
+--Select Tables
+SELECT * FROM "Subcategory";
+SELECT * FROM "Category";
+SELECT * FROM "Contacts";
+SELECT * FROM "Campaign";
